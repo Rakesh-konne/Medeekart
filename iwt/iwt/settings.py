0f0705052dfcc52025986191d8bb19a8f999873a
KONNE RAKESH <113461194+Rakesh-konne@users.noreply.github.com>
2024-09-01 12:02:03 +0530

    Update databaseurl

--- iwt/iwt/settings.py
+++ iwt/iwt/settings.py
@@ -78,17 +78,26 @@ WSGI_APPLICATION = "iwt.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',  # Ensure this is set to PostgreSQL
+#         'NAME': 'dbfordjango',
+#         'USER': 'dbfordjango_user',
+#         'PASSWORD': 'P5ZVoK3WDhTxy8d8AeBNQFmEZZ3lCTYV',
+#         'HOST': 'dpg-cqh0chtds78s73assrhg-a',
+#         'PORT': '5432',
+#     }
+#     }
+
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Ensure this is set to PostgreSQL
-        'NAME': 'dbfordjango',
-        'USER': 'dbfordjango_user',
-        'PASSWORD': 'P5ZVoK3WDhTxy8d8AeBNQFmEZZ3lCTYV',
-        'HOST': 'dpg-cqh0chtds78s73assrhg-a',
-        'PORT': '5432',
-    }
-    }
-    
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # Fetch the database URL from the environment
+        conn_max_age=600
+    )
+}
 
 
 
